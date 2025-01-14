5e23b1bd86620786efb559d68e7f7bf75f9cd0e2
Mohammed Uddin <uddin.mohammed94@gmail.com>
2025-01-14 15:46:32 -0600

    derived year, month, quarter from order date

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.types import StructType, StructField, IntegerType,StringType,DateType
+from pyspark.sql.functions import month, year, quarter
 from config import raw_csv_dir
 
 spark = SparkSession.builder \
@@ -19,5 +20,11 @@ sales_df=spark.read.format("csv")\
     .option("inferschema", "true") \
     .schema(schema)\
     .load(str(raw_csv_dir)+f"/sales.csv.txt")
+
+sales_df.show()
+
+sales_df=sales_df.withColumn("order_year",year(sales_df.order_date))
+sales_df=sales_df.withColumn("order_month",month(sales_df.order_date))
+sales_df=sales_df.withColumn("order_qaurter",quarter(sales_df.order_date))
 
 sales_df.show()
