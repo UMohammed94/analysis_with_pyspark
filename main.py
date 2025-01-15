28508e4ed5660fbe3dbde4305435c6c327eeca19
Mohammed Uddin <uddin.mohammed94@gmail.com>
2025-01-14 22:09:31 -0600

    displays both txt files

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ spark = SparkSession.builder \
     .appName("SalesAnalysis") \
     .getOrCreate()
 
-schema = StructType([
+sales_schema = StructType([
     StructField("product_id",IntegerType(),True),
     StructField("customer_id",StringType(),True),
     StructField("order_date",DateType(),True),
@@ -16,15 +16,29 @@ schema = StructType([
 ])
 
 sales_df=spark.read.format("csv")\
-    .option("header","true")\
+    .option("header","false")\
     .option("inferschema", "true") \
-    .schema(schema)\
+    .schema(sales_schema)\
     .load(str(raw_csv_dir)+f"/sales.csv.txt")
 
-sales_df.show()
+# sales_df.show()
 
 sales_df=sales_df.withColumn("order_year",year(sales_df.order_date))
 sales_df=sales_df.withColumn("order_month",month(sales_df.order_date))
 sales_df=sales_df.withColumn("order_qaurter",quarter(sales_df.order_date))
 
-sales_df.show()
+# sales_df.show()
+
+menu_schema = StructType([
+    StructField("menu_id",IntegerType(),True),
+    StructField("menu_item",StringType(),True),
+    StructField("price",StringType(),True)
+])
+
+menu_df=spark.read.format("csv")\
+    .option("header","false")\
+    .option("inferschema","true")\
+    .schema(menu_schema)\
+    .load(str(raw_csv_dir)+f"/menu.csv.txt")
+
+# menu_df.show()
